e69c8c024ad40bf183a1e6a5c9efb6a3242a69e5
yurubahmed <yurubiftin2002@gmail.com>
2023-02-07 09:04:31 -0500

    commit tree 3730ce6bb45587f2c37131cfd8da253ba0a7b281

--- methods.rb
+++ methods.rb
@@ -1,1 +1,42 @@
 # Your code here!
+def greet_programmer
+    puts "Hello, programmer!"
+end
+greet_programmer
+
+def greet name
+    puts "Hello, #{name}!"
+  end
+  
+  greet("Hello, $name!")
+
+
+def greet_with_default (name= "Hello, programmer!")
+    puts "Hello, #{name}!"
+end
+greet_with_default()
+
+   
+
+
+def add(num1=2 ,num2 =3)
+    return num1 + num2
+end
+
+
+
+def halve(num)
+    if num.is_a?(Integer)
+      num / 2
+    else
+      nil
+    end
+  end
+
+
+
+
+
+
+    
+    
